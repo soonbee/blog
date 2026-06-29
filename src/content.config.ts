aeb38e42c48867e5b25f8268156b8db30672094c
soonbee <maru204@gmail.com>
2026-06-29 14:24:02 +0900

    chore: change post content dir

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { z } from 'astro/zod';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/post', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
